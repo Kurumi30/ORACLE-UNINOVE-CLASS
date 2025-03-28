18536a71636071eeebe2e5dba32a432e424eb77c
Kurumi30 <fernandoshengxinzhu@gmail.com>
2025-03-27 21:04:03 -0300

    adiciona exemplos de inserção e atualização na tabela CLIENTE

--- Aula 20-03-2025.sql
+++ Aula 20-03-2025.sql
@@ -7,10 +7,29 @@ CREATE TABLE FUNC (
 INSERT INTO FUNC
 	(IDFUNC, NOME, CPF)
 VALUES
-	(1, 'Fernando', 12345678901 );
+	(1, 'Fernando', 12345678901);
 
 -- Pode omitir os nomes das colunas, seguindo a ordem da tabela
 INSERT INTO FUNC VALUES (2, 'Marin', NULL);
+
+INSERT INTO CLIENTE
+(IDCLI, NOME, TELEF, TIPOCLI, CNPJ, CPF)
+VALUES
+(7, 'John Doe', '123456789', NULL, '12.345.678/0001-95', 12345678900);
+
+/ -- Ou pode omitir o campo que não vai usar
+
+INSERT INTO CLIENTE
+(IDCLI, NOME, TELEF, CNPJ, CPF)
+VALUES
+(7, 'John Doe', '123456789', '12.345.678/0001-95', 12345678900);
+
+-- Conceito de parâmetros no Oracle
+
+INSERT INTO CLIENTE
+(IDCLI, NOME, TELEF, TIPOCLI, CNPJ, CPF)
+VALUES
+(&IDCLI, '&NOME', '&TELEF', '&TIPOCLI', '&CNPJ', &CPF);
 
 -- Pode usar sem a cláusula WHERE
 UPDATE FUNC SET CPF = 12345678902;
@@ -24,3 +43,4 @@ DELETE FUNC;
 
 DELETE FROM FUNC
 	WHERE IDFUNC = 2;
+-- Campo, operador, valor	
