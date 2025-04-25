eca99212b256c61e8e133a316a14e967e54b1860
Kurumi30 <fernandoshengxinzhu@gmail.com>
2025-04-24 21:58:24 -0300

    adiciona a ordem de execução dos comandos SQL

--- Aula 27-03-2025.sql
+++ Aula 27-03-2025.sql
@@ -5,3 +5,11 @@ COMMIT -- efetiva transações pendentes
 
 -- Os comandos DDL (create, alter, drop, rename, create sequence, etc) não podem ser revertidos com rollback porque possuem o auto-commit ativado.
 -- Os comandos DML (insert, update, delete) podem ser revertidos com rollback
+
+-- A ordem de execução dos comandos:
+-- 1) FROM
+-- 2) WHERE
+-- 3) GROUP BY
+-- 4) SELECT
+-- 5) HAVING
+-- 6) ORDER BY
