dcabf5a04c05d389e014bee76a8dd41babba2228
Kurumi30 <fernandoshengxinzhu@gmail.com>
2025-06-05 20:47:37 -0300

    altera junções de LEFT JOIN para INNER JOIN em consultas SQL e ajusta agrupamentos

--- Exercícios 22-05-2025.sql
+++ Exercícios 22-05-2025.sql
@@ -1,7 +1,7 @@
 -- 46. Consulte o nome dos empregados, salário e nome do departamento da tabela EMP e DEPT considerando apenas quem foi contratado em 81. Utilize o comando LIKE.
 SELECT A.ENAME, A.SAL, A.DEPTNO
 FROM EMP A
-LEFT JOIN DEPT B
+INNER JOIN DEPT B
 ON A.DEPTNO = B.DEPTNO
 WHERE TO_CHAR(A.HIREDATE, 'YYYY') = 1981;
 
@@ -10,45 +10,45 @@ SELECT B.DNAME, MIN(A.SAL) AS "MENOR SALÁRIO", MAX(A.SAL) AS "MAIOR SALÁRIO", 
 FROM EMP A
 INNER JOIN DEPT B
 ON A.DEPTNO = B.DEPTNO
-GROUP BY A.DEPTNO, B.DNAME;
+GROUP BY B.DNAME;
 
 -- 48. Consulte todos os empregados contratados no ano de 87, mostrando seu nome, salário e seu departamento (nome).
 SELECT A.ENAME, A.SAL, B.DNAME
 FROM EMP A
-LEFT JOIN DEPT B
+INNER JOIN DEPT B
 ON A.DEPTNO = B.DEPTNO
 WHERE TO_CHAR(A.HIREDATE, 'YYYY') = 1987;
 
 -- 49. Consulte o nome dos empregados, seu cargo e salário e o nome do seu depto, classificados em ordem crescente por cargo e nome do empregado, nesta ordem.
 SELECT A.ENAME, A.JOB, A.SAL, B.DNAME
 FROM EMP A
-LEFT JOIN DEPT B
+INNER JOIN DEPT B
 ON A.DEPTNO = B.DEPTNO
 ORDER BY A.JOB ASC, A.ENAME ASC;
 
 -- 50. Consulte a quantidade de funcionários por departamento (nome).
-SELECT COUNT(A.ENAME) AS "QUANTIDADE DE FUNCIONÁRIOS", B.DNAME
+SELECT COUNT(*) AS "QUANTIDADE DE FUNCIONÁRIOS", B.DNAME
 FROM EMP A
-LEFT JOIN DEPT B
+INNER JOIN DEPT B
 ON A.DEPTNO = B.DEPTNO
 GROUP BY B.DNAME;
 
 -- 51. Qual o custo dos empregados por departamento (nome).
 SELECT B.DNAME, SUM(A.SAL) AS "CUSTO DOS EMPREGADOS"
 FROM EMP A
-LEFT JOIN DEPT B
+INNER JOIN DEPT B
 ON A.DEPTNO = B.DEPTNO
 GROUP BY B.DNAME;
 
 -- 52. Consulte o número e o nome da categoria do filme e quantos filmes existem na locadora por categoria.
 SELECT A.DESCRICAO, COUNT(B.CODCATEG)
 FROM CATEGORIA A
-LEFT JOIN FILME B
+INNER JOIN FILME B
 ON A.CODCATEG = B.CODCATEG
 GROUP BY A.DESCRICAO;
 
 -- 53. Consulte quantos filmes cada cliente alugou, mostrando o pré-nome do cliente e a quantidade de filmes ao lado.
-SELECT B.PRENOME, SUM(A.NUMDVD)
+SELECT B.PRENOME, COUNT(A.NUMDVD)
 FROM CLIENTE_DVD A
 INNER JOIN CLIENTE B
 ON A.CODCLI = B.CODCLI
@@ -85,4 +85,5 @@ FROM PAGTO A
 INNER JOIN PROF B
 ON A.IDPROF = B.IDPROF
 WHERE A.DTPAGTO BETWEEN '01/01/2008' AND '30/06/2008'
-GROUP BY B.NOME;
+GROUP BY B.NOME
+ORDER BY B.NOME;
